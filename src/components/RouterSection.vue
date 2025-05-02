c30b751ca94cacec9b975fae7af10cecba45790c
mekanio20 <mrxyok138@gmail.com>
2025-05-02 12:48:38 +0500

    news detail completed

---
+++ src/components/RouterSection.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="flex items-center space-x-4">
+        <!-- Arrow Left -->
+        <svg width="11" height="7" viewBox="0 0 11 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g clip-path="url(#clip0_174_4285)">
+                <path
+                    d="M-0.00202847 3.49938C0.0023222 3.37168 0.049715 3.24872 0.133112 3.14878L2.79952 0.261235C2.89412 0.169365 3.02103 0.114211 3.15573 0.106422C3.29044 0.098634 3.42339 0.138763 3.52892 0.219062C3.62446 0.32184 3.67678 0.454721 3.67582 0.59212C3.67486 0.729519 3.62069 0.861722 3.52373 0.963275L1.6491 2.99084H10.4999C10.634 3.00116 10.7591 3.0593 10.8503 3.15369C10.9415 3.24808 10.9922 3.37179 10.9922 3.50021C10.9922 3.62863 10.9415 3.75235 10.8503 3.84673C10.7591 3.94112 10.634 3.99926 10.4999 4.00958H1.65083L3.52546 6.03714C3.61567 6.14178 3.66542 6.27286 3.66637 6.40837C3.66731 6.54388 3.6194 6.67558 3.53066 6.78136C3.42576 6.8631 3.29248 6.90414 3.15732 6.89633C3.02216 6.88852 2.89504 6.83241 2.80125 6.73919L0.134845 3.85164C0.043788 3.75521 -0.00518227 3.62917 -0.00202847 3.49938Z"
+                    fill="black" />
+            </g>
+            <defs>
+                <clipPath id="clip0_174_4285">
+                    <rect width="11" height="7" fill="white" transform="matrix(-1 0 0 1 11 0)" />
+                </clipPath>
+            </defs>
+        </svg>
+        <router-link :to="route" class="font-inter font-light text-sm uppercase">
+            {{ prevRoute }}
+        </router-link>
+        <span class="font-inter font-light text-sm uppercase">/</span>
+        <p class="font-inter font-light text-sm uppercase">
+            {{ nextRoute }}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        route: {
+            name: String,
+            required: true
+        },
+        prevRoute: {
+            type: String,
+            required: true
+        },
+        nextRoute: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
--- src/views/NewsDetail.vue
+++ src/views/NewsDetail.vue
@@ -5,8 +5,45 @@
         <!-- Navbar -->
         <Navbar />
         <!-- Main -->
-        <div class="pb-40">
-
+        <div class="pb-40 pt-14">
+            <div class="container">
+                <RouterSection route="/news" prevRoute="Dükanlar" nextRoute="LUSIO представляет Slow Life" />
+                <div class="py-10 flex items-center space-x-6 justify-center">
+                    <div class="border border-arkac-gray-200 rounded-full px-10 py-3 font-inter text-sm uppercase">
+                        Täzelikler
+                    </div>
+                    <div class="border border-arkac-gray-200 rounded-full px-10 py-3 font-inter text-sm uppercase">
+                        15 Aprel, 2025
+                    </div>
+                </div>
+                <h1 class="font-inter font-medium text-[52px] text-center uppercase">Межсезонная распродажа в ELIS</h1>
+                <div class="w-full my-14 h-[460px]">
+                    <img class="w-full h-full object-cover rounded-lg" src="/imgs/news-1.png">
+                </div>
+                <div class="pt-4">
+                    <h3 class="font-inter font-semibold text-2xl">Новая линия украшений UNOde50 Ser Camaleónica
+                        призывает нас к переменам.</h3>
+                    <p class="font-inter font-medium text-lg py-10">
+                        Быть «хамелеоном» – значит быть разным и готовым открывать новые грани своей личности.
+                        Настроение волшебства и чудесных превращений в новых украшениях UNOde50 создают удивительные
+                        кристаллы-хамелеоны. Позолоченные или посеребренные кольца, серьги, браслеты и ожерелья с
+                        кристаллами-хамелеонами – это яркий и стильный акцент, который гармонично впишется как в
+                        повседневный образ, так и в вечерний наряд. Наденьте его – и пусть каждый момент станет
+                        волшебным.
+                    </p>
+                </div>
+                <div class="pt-20">
+                    <h2 class="font-inter font-medium text-4xl uppercase">Meňzeş dükanlar</h2>
+                    <div class="w-full py-6 rounded-xl mt-10 relative overflow-hidden">
+                        <Swiper :modules="modules" :slides-per-view="4" :space-between="40" :loop="true">
+                            <SwiperSlide v-for="item in news" :key="item.id">
+                                <NewsCard :id="item.id" :title="item.title" :date="item.date" :image="item.image"
+                                    :category="item.category" />
+                            </SwiperSlide>
+                        </Swiper>
+                    </div>
+                </div>
+            </div>
         </div>
         <!-- Footer -->
         <Footer />
@@ -17,12 +54,52 @@
 import Header from '@/components/Header.vue';
 import Navbar from '@/components/Navbar.vue';
 import Footer from '@/components/Footer.vue';
+import RouterSection from '@/components/RouterSection.vue';
+import NewsCard from '@/components/NewsCard.vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 export default {
     name: 'NewsDetail',
     components: {
+        Swiper,
+        SwiperSlide,
         Header,
         Navbar,
-        Footer
+        Footer,
+        RouterSection,
+        NewsCard
+    },
+    data() {
+        return {
+            news: [
+                {
+                    title: 'Все для твоего яркого макияжа по акции 1+1=3!',
+                    category: 'Çäreler',
+                    date: '23.03.2025',
+                    image: 'https://images.pexels.com/photos/3373736/pexels-photo-3373736.jpeg'
+                },
+                {
+                    title: 'В SUNLIGHT -50% на всё!',
+                    category: 'Täzelikler',
+                    date: '23.03.2025',
+                    image: 'https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg'
+                },
+                {
+                    title: 'Все для твоего яркого макияжа по акции 1+1=3!',
+                    category: 'Çäreler',
+                    date: '23.03.2025',
+                    image: 'https://images.pexels.com/photos/3738088/pexels-photo-3738088.jpeg'
+                },
+                {
+                    title: 'В SUNLIGHT -50% на всё!',
+                    category: 'Täzelikler',
+                    date: '23.03.2025',
+                    image: 'https://images.pexels.com/photos/1162983/pexels-photo-1162983.jpeg'
+                }
+            ]
+        }
     }
 }
-</script>   
+</script>
