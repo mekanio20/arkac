0d438f5c17a33f5c2278fa0dcf179cdc96d19c7a
mekanio20 <mrxyok138@gmail.com>
2025-06-10 17:53:39 +0500

    update cards

--- src/components/ShopCard.vue
+++ src/components/ShopCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <router-link :to="`/shop/detail/${id}`" class="h-[280px] sm:h-[300px] md:h-[330px] px-4 md:px-6 py-4 sm:py-5 md:py-6 border border-arkac-gray-400 rounded-2xl flex flex-col sm:items-start items-center justify-between space-y-0 sm:space-y-4">
+    <router-link :to="`/shop/detail/${id}`" class="h-[300px] sm:h-[320px] md:h-[350px] px-4 md:px-6 py-4 sm:py-5 md:py-6 border border-arkac-gray-400 rounded-2xl flex flex-col sm:items-start items-center justify-between space-y-0 sm:space-y-8">
         <div class="flex space-x-2">
             <span class="px-2 sm:px-3 py-1 text-xs sm:text-sm bg-arkac-blue-300 text-arkac-blue-200 rounded-md">
                 {{ floor }}-gat
@@ -11,8 +11,8 @@
                 </span>
             </span>
         </div>
-        <div class="flex items-center justify-center w-24 h-24 sm:w-32 sm:h-32">
-            <img :src="image" alt="Logo" class="w-full h-full rounded-full" />
+        <div class="self-center flex items-center justify-center w-full h-40 pr-4">
+            <img :src="image" alt="Logo" class="w-full h-full object-cover" />
         </div>
 
         <div class="sm:text-start text-center">
