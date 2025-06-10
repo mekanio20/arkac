c79674395de5f0a30b58828637139aa29f8af6f5
mekanio20 <mrxyok138@gmail.com>
2025-06-10 18:12:42 +0500

    test

--- src/views/View360.vue
+++ src/views/View360.vue
@@ -1,14 +1,6 @@
 <template>
     <div class="iframe-container">
-        <iframe 
-            class="w-full h-screen"
-            src="https://yakyn.biz/360/arkac-sowda-dync-alys-merkezi-916/index.htm"
-            frameborder="0"
-            allowfullscreen
-            loading="lazy"
-            title="360 View"
-            sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
-        ></iframe>
+        <iframe src='https://yakyn.biz/360/917/index.htm' width='100%' height='100%' allowFullScreen />
     </div>
 </template>
 
